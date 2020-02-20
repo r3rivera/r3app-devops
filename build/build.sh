cd7423c879ac2b05ffe59386bfa552b3bfd57ae1
r3rivera <r.ryan.rivera@outlook.com>
2020-02-19 21:48:33 -0600

    Added build

---
+++ build/build.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+#dir `date`
+echo "## Building the project of SMIS..."
+
+echo "## Displaying Java Version..."
+java -version
+
+echo "## Displaying Maven Version..."
+mvn --version
+
+echo "## Displaying GIT Version..."
+git --version
