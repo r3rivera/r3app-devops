6eae4f43b239b1d6391b8014c74b17e0a1b2414a
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-03-05 21:40:45 -0600

    Modify build

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,7 @@
 !#/bin/bash
 #dir `date`
 echo "## Building the project of ${APP_NAME}..."
+echo "## Workspace is ${WORKSPACE}"
 
 echo "## Displaying Java Version..."
 java -version | head -n 1
@@ -10,3 +11,7 @@ mvn --version | head -n 1
 
 echo "## Displaying GIT Version..."
 git --version | head -n 1
+
+echo "########## Start Building the Application ##########"
+GIT_COMMITID="$(git log | head -n 1)"
+echo "Latest Git Commit is ${GIT_COMMITID}"
