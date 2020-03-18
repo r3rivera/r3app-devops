a0e7f860e7c2a5c625d485a3f954bbe25541eb69
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-03-17 21:26:18 -0500

    Fix something

--- deploy/deploy-app.sh
+++ deploy/deploy-app.sh
@@ -6,12 +6,15 @@ echo "deploy-app.sh ::: Target File is [ ${TARGET_FILE} ]"
 
 #1. Get the environment config from S3
 echo "deploy-app.sh ::: Configuration bucket is ::: ${S3_APPL_CONFIG_BUCKET}"
-aws s3 cp ${S3_APPL_CONFIG_BUCKET} .
-if [[ ! -f env-config.properties ]]
+
+CONFIG_NAME=$(echo ${S3_APPL_CONFIG_BUCKET} | cut -d '/' -f 5)
+echo "deploy-app.sh ::: Configuration Name is ::: ${CONFIG_NAME}"
+aws s3 cp ${CONFIG_NAME} ${S3_APPL_CONFIG_BUCKET}
+if [[ ! -f ${CONFIG_NAME} ]]
 then
   echo "Environment configuration is missing!"
   exit 1
 fi 
-source ./env-config.properties
+source ${CONFIG_NAME}
 echo "deploy-app.sh ::: EB Application Name :: ${dev-smis-eb-appl-name}"
 echo "deploy-app.sh ::: EB Environment Name :: ${dev-smis-eb-env-name}"
