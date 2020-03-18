0d62d3939353927a4206894ac0f570fdf5bc8098
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-03-17 20:59:53 -0500

    Added deploy to get the app config properties

--- deploy/deploy-app.sh
+++ deploy/deploy-app.sh
@@ -5,4 +5,13 @@ echo "deploy-app.sh ::: S3 Target Bucket Prefix is [ ${S3_TARGET_BUCKET_PREFIX} 
 echo "deploy-app.sh ::: Target File is [ ${TARGET_FILE} ]"
 
 #1. Get the environment config from S3
-echo "Configuration bucket is ::: ${S3_APPL_CONFIG_BUCKET}"
+echo "deploy-app.sh ::: Configuration bucket is ::: ${S3_APPL_CONFIG_BUCKET}"
+aws s3 cp ${S3_APPL_CONFIG_BUCKET} .
+if [[ ! -f env-config.properties ]]
+then
+  echo "Environment configuration is missing!"
+  exit 1
+fi 
+source ./env-config.properties
+echo "deploy-app.sh ::: EB Application Name :: ${dev-smis-eb-appl-name}"
+echo "deploy-app.sh ::: EB Environment Name :: ${dev-smis-eb-env-name}"
