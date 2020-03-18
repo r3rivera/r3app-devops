f4eb0aee737bcc54d2ea48ee9109bd912f93163c
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-03-17 20:45:41 -0500

    Added cloudformation for the bukcet config

--- deploy/deploy-app.sh
+++ deploy/deploy-app.sh
@@ -3,3 +3,6 @@ echo "deploy-app.sh ::: Deploying applications..."
 echo "deploy-app.sh ::: Target Environment is :: $1"
 echo "deploy-app.sh ::: S3 Target Bucket Prefix is [ ${S3_TARGET_BUCKET_PREFIX} ]"
 echo "deploy-app.sh ::: Target File is [ ${TARGET_FILE} ]"
+
+#1. Get the environment config from S3
+echo "Configuration bucket is ::: ${S3_APPL_CONFIG_BUCKET}"
