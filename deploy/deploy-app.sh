a5863e11ed9933b9736408d31af73e1eb3b4dba9
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-03-21 14:16:01 -0500

    Added code for aws

--- deploy/deploy-app.sh
+++ deploy/deploy-app.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
 function getApplConfigurationAndCreateApp() {
-    echo "::: Deploying applications..."
+
     echo "::: Target Environment is :: $1"
     echo "::: S3 Target Bucket Prefix is [ ${S3_TARGET_BUCKET_PREFIX} ]"
+    echo "::: S3 Target Bucket SUB Prefix is [ ${S3_BUCKET_PREFIX_SUB} ]"
     echo "::: Target File is [ ${TARGET_FILE} ]"
+
+    if [[ -z ${S3_APPL_CONFIG_BUCKET} ]]
+    then
+        echo "S3 bucket with configuration is not available or provided!"
+        exit 1;
+    fi
 
     #1. Get the environment config from S3
     echo "::: Configuration bucket is ::: ${S3_APPL_CONFIG_BUCKET}"
 
     CONFIG_NAME=$(echo ${S3_APPL_CONFIG_BUCKET} | cut -d '/' -f 5)
     echo "::: Configuration Name is ::: ${CONFIG_NAME}"
-    aws s3 cp ${S3_APPL_CONFIG_BUCKET} ${CONFIG_NAME}
+    aws s3 cp ${S3_APPL_CONFIG_BUCKET} ${CONFIG_NAME} || exit 1
     if [[ ! -f ./${CONFIG_NAME} ]]
     then
         echo "::: Environment configuration is missing!" && exit 1
-
     fi 
     source ./${CONFIG_NAME}
     echo "::: EB Application Name :: ${dev_smis_eb_appl_name}"
-    #echo "::: EB Application Description :: ${dev_smis_eb_appl_description}"
+    echo "::: EB Application Description :: ${dev_smis_eb_appl_description}"
     echo "::: EB Environment Name :: ${dev_smis_eb_env_name}"
     
-    createApplication ${dev_smis_eb_appl_name} 
+    #Use the bucket folder as the version label
+    createApplicationVersion ${dev_smis_eb_appl_name} \
+                             ${dev_smis_eb_appl_description} \ 
+                             ${S3_BUCKET_PREFIX_SUB} \
+                             "${S3_TARGET_BUCKET_PREFIX}/${TARGET_FILE}"
 }
 
 
@@ -43,9 +53,6 @@ function checkApplStatusAndWait() {
        fi
 
         echo "::: Checking the status of the ${dev_smis_eb_env_name} environment..."
-        #EB_APPL_STATUS=$(aws elasticbeanstalk describe-environment-health --environment-name ${dev_smis_eb_env_name} \ 
-        #   --attribute-names Status | python3 -c 'import sys, json; print json.load(sys.stdin)["Status"])
-
         EB_APPL_STATUS=$(aws elasticbeanstalk describe-environment-health --environment-name rcgc-smis-dev-01-webapp --attribute-names Status \
                | grep Status | cut -d ' ' -f 2)
         echo "::: The status of the ${dev_smis_eb_env_name} is currently ${EB_APPL_STATUS}..."
@@ -53,17 +60,34 @@ function checkApplStatusAndWait() {
 }
 
 
-function createApplication() {
-    echo "::: Creating application of $1..."
-    EB_APPL_NAME=$(aws elasticbeanstalk describe-applications --application-names $1| grep $1)
-    if [[ ${EB_APPL_NAME} == "" ]]
+function createApplicationVersion() {
+    APP_NAME_EB=$1
+    APP_DESC_EB=$2
+    APP_ARTIFACT_LABEL=$3
+    APP_ARTIFACT_FULLPATH=$4
+    echo "::: Creating application of ${APP_NAME_EB}..."
+    echo "::: Application description is ${APP_DESC_EB}..."
+    echo "::: Application version label is ${APP_ARTIFACT_LABEL}"
+    echo "::: Application artifcat version source is ${APP_ARTIFACT_FULLPATH}"
+
+    APP_ACCT_ID=$(aws sts get-caller-identity | grep Arn: | cut -d ':' -f 6)
+    echo "::: AWS Account ID is ${APP_ACCT_ID}"
+
+    EB_APPL_NAME=$(aws elasticbeanstalk describe-applications --application-names ${APP_NAME_EB}| grep ${APP_NAME_EB})
+    if [[ -z ${EB_APPL_NAME} ]] #Application Name exist
     then 
-        echo "::: Application of $1 does not exist..."
-        exit 1
-        #aws elasticbeanstalk create-application --application-name $1 --description $2
+        echo "::: Uploading application version of ${APP_ARTIFACT_LABEL} into the S3 bucket"
+        #aws s3 cp s3://
+
+        echo "::: Application of ${APP_NAME_EB} does not exist..."
+        aws elasticbeanstalk create-application-version --application-name ${APP_NAME_EB} --description ${APP_DESC_EB} \
+        --version-label ${APP_ARTIFACT_LABEL} --source-bundle S3Bucket="${APP_ARTIFACT_S3BUCKET}",S3Key="${APP_ARTIFACT_S3KEY}" \
+        --auto-create-application || exit 1
+
     else
-        echo "::: Application of $1 is exist..."
-        echo ${EB_APPL_NAME}
+        echo "::: Application of ${APP_NAME_EB} is exist..."
+        echo "::: Application details is ${EB_APPL_NAME}"
+
     fi
 }
 
